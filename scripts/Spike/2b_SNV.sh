bf0447184b77cfd3d1261b8383bb55f9792d359e
Philippe Sanio <philippe.sanio@gmail.com>
2023-08-31 23:17:48 +0200

    Update 2b_SNV.sh

--- scripts/Spike/2b_SNV.sh
+++ scripts/Spike/2b_SNV.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Input vairables are: READ_LENGTH, VAF, MERGED_VCF, MOD_BAM, REF_FASTA and OUTPUT_DIR
+# Input vairables are: READ_LENGTH, VAF, MERGED_VCF, MOD_BAM and OUTPUT_DIR
 # READ_LENGTH can be either SHORT or LONG
 
 READ_LENGTH=$1
 VAF=$2
 MERGED_VCF=$3
 MOD_BAM=$4
-REF_FASTA=$5
-OUTPUT_VCF=$6
+OUTPUT_VCF=$5
 
 # activate conda env which uses python 3.6 called mosaicSim
 conda activate mosaicSim
