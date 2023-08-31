6413a9fbee899598367ff3fac47a53094bb5c349
Philippe Sanio <philippe.sanio@gmail.com>
2023-08-31 23:21:00 +0200

    Update 2b_re-genotyping_main.sh

--- scripts/Spike/2b_re-genotyping_main.sh
+++ scripts/Spike/2b_re-genotyping_main.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Input vairables are: VARIANT, VAF, VCF_1, VCF_2, MOD_BAM, REF_FASTA, OUTPUT_DIR, READ_LENGTH
+# Input variables are: VARIANT, VAF, VCF_1, VCF_2, MOD_BAM, OUTPUT_DIR, READ_LENGTH
 # VARIANT can be either SNV or SV
 # READ_LENGTH can be either SHORT or LONG for SVs
 
 # Example command:
 # For SNV:
-# ./2b_re-genotyping_main.sh SNV 0.1 /path/to/vcf1.vcf /path/to/vcf2.vcf /path/to/mod.bam /path/to/ref.fasta /path/to/output_dir
+# ./2b_re-genotyping_main.sh SNV 0.1 /path/to/vcf1.vcf /path/to/vcf2.vcf /path/to/mod.bam /path/to/output_dir
 # or 
 # For SV:
 # ./2b_re-genotyping_main.sh SV 0.1 /path/to/vcf1.vcf /path/to/vcf2.vcf /path/to/mod.bam /path/to/output_dir SHORT
@@ -17,9 +17,8 @@ VAF=$2
 VCF_1=$3
 VCF_2=$4
 MOD_BAM=$5
-REF_FASTA=$6
-OUTPUT_DIR=$7
-READ_LENGTH=$8
+OUTPUT_DIR=$6
+READ_LENGTH=$7
 MERGED_VCF="${OUTPUT_DIR}/merged.vcf"
 OUTPUT_VCF="${OUTPUT_DIR}/output_genotypes.vcf"
 OUTPUT_VCF_FILTERED="${OUTPUT_DIR}/output_genotypes_filtered.vcf"
@@ -29,7 +28,7 @@ bcftools merge $VCF_1 $VCF_2 -o $MERGED_VCF --use-header
 
 if [ "$VARIANT" = "SNV" ]; then
     echo "Input is 'SNV'"
-    ./2b_SNV.sh $READ_LENGTH $VAF $MERGED_VCF $MOD_BAM $REF_FASTA $OUTPUT_VCF
+    ./2b_SNV.sh $READ_LENGTH $VAF $MERGED_VCF $MOD_BAM $OUTPUT_VCF
 elif [ "$VARIANT" = "SV" ]; then
     echo "Input is 'SV'"
     ./2b_SV.sh $READ_LENGTH $VAF $MERGED_VCF $MOD_BAM $OUTPUT_VCF
