5ac22d9c6c4794697e7285b2c0a6daafdaa1d1fa
zhengxinchang <zhengxinchang1993@163.com>
2023-09-01 09:17:57 -0500

    replace the samtools to bcftools to make sure it can run smoothly.

--- scripts/Spike/2b_SNV.sh
+++ scripts/Spike/2b_SNV.sh
@@ -8,8 +8,9 @@ VAF=$2
 MERGED_VCF=$3
 MOD_BAM=$4
 OUTPUT_VCF=$5
-
+REFERENCE=$6
 # activate conda env which uses python 3.6 called mosaicSim
 conda activate mosaicSim
 # call samtools mpileup to genotypes the variants
-samtools mpileup $MOD_BAM | bcftools call -mv -Oz -o $OUTPUT_VCF
+#samtools mpileup $MOD_BAM | bcftools call -mv -Oz -o $OUTPUT_VCF
+bcftools  mpileup  -Ou -f $REFERENCE $MOD_BAM  |  bcftools call -mv -Oz -o $OUTPUT_VCF
