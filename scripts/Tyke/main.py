dc9d01dbc265bcbc34cf4e11e96640e174050e04
Joyjit Daw <joyjit.daw@nanoporetech.com>
2023-08-31 21:10:57 -0500

    fix candidate read selection
    1. select primary reads only
    2. avoid reads that have alrady been edited for another variant

--- scripts/Tyke/main.py
+++ scripts/Tyke/main.py
@@ -45,6 +45,7 @@ def argument_parser():
     return parser.parse_args()
 
 def vcf_number_variants(input_vcf_file,input_bam_file, refs, outfile):
+    processed_read_ids = set()
     """return the number of each variant seprately"""
     bamfile = pysam.AlignmentFile(input_bam_file, 'rb')
     #print(variant_reads)
@@ -63,26 +64,39 @@ def vcf_number_variants(input_vcf_file,input_bam_file, refs, outfile):
                     end_position = int(obj.POS) + variant_len
                     variant_seq = obj.ALT[:variant_len]
                     #print(start_position,end_position)
+                    svtype = "INS"
+                    variant_len = 100
+                    variant_seq = 'T' * variant_len
+                    #print(f"{chromosome}:{start_position} {variant_len} {variant_seq}")
                     variant_reads = []
                     for read in bamfile.fetch(chromosome, start_position,end_position):
-                        if read.query_sequence is not None:
+                        if read.query_sequence is not None and read.flag == 0 and read.query_name not in processed_read_ids:
                             variant_reads.append(read)
+                    no_reads=max(1,math.ceil(len(variant_reads)*obj.AF))
+                    if no_reads > len(variant_reads):
+                        #print(f"Coverage {len(variant_reads)} is below required minimum reads for variant {chromosome}:{start_position} AF {obj.AF}")
+                        continue
+                    random.shuffle(variant_reads)
                     #print(f"DR={obj.DR},AF={obj.AF},SVTYPE={obj.SVTYPE},CHROMOE={obj.CHROM},POS={obj.POS},SV_LENGTH={obj.SVLEN}")
-                    no_reads=max(1,math.ceil(obj.DR*obj.AF))
-                    if len(variant_reads) > no_reads:
-                        selected_elements = random.sample(variant_reads, no_reads)
-                    # print(f"{obj.DR}AF={obj.AF},SVTYPE={obj.SVTYPE},CHROMOE={obj.CHROM},POS={obj.POS},SV_LENGTH={obj.SVLEN},SV={obj.ALT}")
-                    #print(selected_elements)
+                    #print(f"{obj.DR}AF={obj.AF},SVTYPE={obj.SVTYPE},CHROMOE={obj.CHROM},POS={obj.POS},SV_LENGTH={obj.SVLEN},SV={obj.ALT}")
                     ref_seq = refs[read.reference_name]
-                    for read in selected_elements:
-                        if not read.query_sequence:
-                            continue
-                        new_seq, new_qual = edit_read(ref_seq, read.reference_start, read.query_sequence, read.query_qualities, read.cigartuples, start_position, variant_len, svtype, variant_seq)
-                        if new_seq:
-                            #write_fastx_record(out_fh, read.query_name, read.query_sequence, read.query_qualities)
-                            #query_name = f"{read.query_name}|{read.reference_name}|{start_position}|{svtype}|{variant_len}"
-                            query_name = read.query_name
-                            write_fastx_record(out_fh, query_name, new_seq, new_qual)
+                    num_reads_edited = 0
+                    for read in variant_reads:
+                        read_edited = False
+                        if num_reads_edited < no_reads:
+                            new_seq, new_qual = edit_read(ref_seq, read.reference_start, read.query_sequence, read.query_qualities, read.cigartuples, start_position, variant_len, svtype, variant_seq)
+                            if new_seq:
+                                #write_fastx_record(out_fh, read.query_name, read.query_sequence, read.query_qualities)
+                                #query_name = f"{read.query_name}|{read.reference_name}|{start_position}|{svtype}|{variant_len}"
+                                query_name = read.query_name
+                                write_fastx_record(out_fh, query_name, new_seq, new_qual)
+                                num_reads_edited += 1
+                                read_edited = True
+                                processed_read_ids.add(read.query_name)
+                        #if not read_edited:
+                        #    write_fastx_record(out_fh, read.query_name, read.query_sequence, read.query_qualities)
+                    if num_reads_edited != no_reads:
+                        print("Could not fulfill required minimum edited reads for variant at {chromosome}:{start_position}:{end_position} type {svtype}")
 
     bamfile.close()
     return
