db8c307757585b832c67ac48eae2573fe5ac7fe5
Philippe Sanio <psanio10@gmail.com>
2024-03-28 15:32:51 +0100

    Updated the long read variants regenotyping path
    Merged VCF is collapsed after merging. Using merged vcf instead of two seperate vcfs for regenotyping. Added truvari as requirement

--- scripts/Spike/2b_re-genotyping_main.sh
+++ scripts/Spike/2b_re-genotyping_main.sh
@@ -20,7 +20,9 @@ MOD_BAM=$5
 OUTPUT_DIR=$6
 READ_LENGTH=$7
 MERGED_VCF="${OUTPUT_DIR}/merged.vcf.gz"
-OUTPUT_VCF="${OUTPUT_DIR}/output_genotypes.vcf"
+COLLAPSED_MERGED_VCF="${OUTPUT_DIR}/collapsed_merged.vcf.gz"
+COLLAPSED_VARIANTS_VCF="${OUTPUT_DIR}/collapsed_variants.vcf.gz"
+OUTPUT_VCF="${OUTPUT_DIR}/output_regenotyped.vcf.gz"
 OUTPUT_VCF_FILTERED="${OUTPUT_DIR}/output_genotypes_filtered.vcf"
 REFERENCE=${8}
 
@@ -28,13 +30,15 @@ mkdir -p $OUTPUT_DIR
 
 # Merge the two VCF files with bcf tools
 bcftools merge --force-samples -O z --write-index -o $MERGED_VCF $VCF_A $VCF_B
+# Collapse variants with truvari
+truvari collapse -i $MERGED_VCF -o $COLLAPSED_MERGED_VCF -c $COLLAPSED_VARIANTS_VCF
 
 if [ "$VARIANT" = "SNV" ]; then
     echo "Input is 'SNV'"
-    ./2b_SNV.sh $READ_LENGTH $VAF $MERGED_VCF $MOD_BAM $OUTPUT_VCF $REFERENCE
+    ./2b_SNV.sh $READ_LENGTH $VAF $COLLAPSED_MERGED_VCF $MOD_BAM $OUTPUT_VCF $REFERENCE
 elif [ "$VARIANT" = "SV" ]; then
     echo "Input is 'SV'"
-    ./2b_SV2.sh $READ_LENGTH $VAF $VCF_A $VCF_B $OUTPUT_VCF "${OUTPUT_DIR}/output_genotypes" $REFERENCE "Spike_out"
+    ./2b_SV2.sh $READ_LENGTH $VAF $COLLAPSED_MERGED_VCF $OUTPUT_VCF "${OUTPUT_DIR}/output_genotypes" $REFERENCE "Spike_out"
 else
     echo "Error: Invalid input"
 fi
